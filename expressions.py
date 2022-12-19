a3a45fa1542b82b7ff313245ef13de97ec189b33
Stha-Sujan <mesujanstha143@gmail.com>
2022-12-19 12:14:27 +0545

    python basics

---
+++ expressions.py
@@ -0,0 +1,40 @@
+print( 2 + 2 )
+# prints the 4 as output
+
+print( 5 - 2 )
+# prints the 3 as output
+
+print( 3 * 7 )
+#prints the 21 as output
+
+print ( 2 + 3 * 6 )
+# gives 20 as output 
+# first does the multiplication of 3*6 as 18
+# then addition is done of 18+2
+
+print ( (2+3) * 6 )
+# gives 30 as output 
+# first does the addition of 2+3=5 and 
+# then does the multiplication 5*6=30
+
+## print ( 5 + ) writing such expression gives the invalid syntax
+
+# we can concatinate the two multiple strings as expressions of one strings
+print ( 'Sujan' + 'Shrestha')
+# gives SujanShrestha as output in one string
+
+## string replication
+
+print ('Sujan' * 3)
+# gives SujanSujanSujan as output which multiplies the strings 3 times
+
+
+#assignment statements
+
+spam = 4
+egg = 5
+print(spam + egg)
+
+#gives 9 as output 4+5=9
+
+
